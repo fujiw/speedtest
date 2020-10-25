4ad63dbc51f0c311f84d359da2f07e9760186e14
fujiw <fujiw10@gmail.com>
2020-10-26 01:00:22 +0900

    fix

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,33 @@
 pub mod request;
 pub mod response;
 
+use influxdb::{Client, Query, Timestamp};
+use std::process::Command;
+use serde::{Serialize, Deserialize};
+use serde_json;
+use chrono::prelude::*;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Response {
+    download: f64,
+    upload: f64,
+    ping: f64,
+    timestamp: DateTime<Utc>,
+}
 pub fn hello() -> String {
     "hello".to_string()
+}
+
+pub fn run() {
+    //let client = Client::new("http://localhost:8086", "test");
+    let out = Command::new("speedtest")
+        .args(&["--json"])
+        .output()
+        .expect("failed to start");
+
+    let str = std::str::from_utf8(&out.stdout).unwrap();
+    let deserialized: Response = serde_json::from_str(str).unwrap();
+
+    println!("deserialized = {:?}", deserialized);
+    //return client;
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,6 @@ fn main() {
 
     assert_eq!(speedtest::client::request::hello(), "request!".to_string());
     assert_eq!(speedtest::client::response::hello(), "response!!".to_string());
+
+    speedtest::client::run();
 }
