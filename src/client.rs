ff724000b2f1dd97c328cf6368205d778dca3169
fujiw <fujiw10@gmail.com>
2020-10-30 01:53:43 +0900

    fix

--- src/client.rs
+++ src/client.rs
@@ -13,6 +13,7 @@ struct Response {
     download: f64,
     upload: f64,
     ping: f64,
+    #[serde(rename="timestamp")]
     time: DateTime<Utc>,
 }
 
@@ -29,21 +30,12 @@ pub async fn run() {
         .expect("failed to start");
 
     let str = std::str::from_utf8(&out.stdout).unwrap();
-    //let deserialized: Response = serde_json::from_str(str).unwrap();
+    let mut deserialized: Response = serde_json::from_str(str).unwrap();
     //println!("deserialized = {:?}", deserialized);
 
-    let resp = Response{
-        download: 70087681.29487312,
-        upload: 95260765.00399733,
-        time: Utc::now(),
-        ping: 3.789,
-    };
-
-    //let write_result = client.query(&resp.into_query("status")).await;
+    let _write_result = client.query(&deserialized.into_query("status")).await;
 
     let read_query = Query::raw_read_query("select * from status");
     let read_result = client.query(&read_query).await;
     println!("{}", read_result.unwrap());
-
-    //return client;
 }
